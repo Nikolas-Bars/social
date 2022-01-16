f44ea7f60e2a6dbe99a557479ef5be57fb60ea0d
Nikolas-Bars <kasp2409@mail.ru>
2022-01-16 22:04:24 +0300

    lesson 10

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
+import Footer from "./Footer";
+
+function App() {
+    return (
+
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
