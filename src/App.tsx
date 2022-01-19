2b52157cd506f0ae5759dc9f83c6adacd1071260
Nikolas-Bars <kasp2409@mail.ru>
2022-01-19 20:33:58 +0300

    lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+import Rica from './img/Rica.jpg'
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 function App() {
     return (
 
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className={"app-wrapper"}>
+            <Header />
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
