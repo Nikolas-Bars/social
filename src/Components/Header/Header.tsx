ed01ae7fce5ba35d3ee4f820b31c712d68c00508
Nikolas-Bars <kasp2409@mail.ru>
2022-01-20 22:23:38 +0300

    lesson 20 - style of NavLink

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from './Header.module.css'
+import cat from './../../img/cat.png'
 
 const Header =()=>{
     return(
         <header className={s.header}>
-        img
+        <img src={cat} className={s.imghead}/>
     </header>)
 }
 
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,21 +5,21 @@ import {NavLink} from "react-router-dom";
 
 const Navbar =()=>{
     return(
-        <nav className={s.nav}>
-            <div>
-                <NavLink to={'/profile'}>Profile</NavLink>
+        <nav className={s.navbar}>
+            <div className={s.nav}>
+                <NavLink to={'/profile'} style={({isActive})=>({color: isActive ? 'gold' : 'gray'})}>Profile</NavLink>
             </div>
-            <div className={`${s.i1} ${s.i2}`}>
-                <NavLink to={'/dialogs'}>Messages</NavLink>
+            <div className={`${s.i1} ${s.nav}`}>
+                <NavLink to={'/dialogs'} style={({isActive})=>({color: isActive ? 'gold' : 'gray'})}>Messages</NavLink>
             </div>
-            <div>
-                <NavLink to={'/news'}>News</NavLink>
+            <div className={s.nav}>
+                <NavLink to={'/News'} style={({isActive})=>({color: isActive ? 'gold' : 'gray'})}>News</NavLink>
             </div>
-            <div>
-                <NavLink to={'/music'}>Music</NavLink>
+            <div className={s.nav}>
+                <NavLink to={'/Music'} style={({isActive})=>({color: isActive ? 'gold' : 'gray'})}>Music</NavLink>
             </div>
-            <div>
-                <NavLink to={'/settings'}>Settings</NavLink>
+            <div className={s.nav}>
+                <NavLink to={'/Settings'} style={({isActive})=>({color: isActive ? 'gold' : 'gray'})}>Settings</NavLink>
             </div>
         </nav>
     )
