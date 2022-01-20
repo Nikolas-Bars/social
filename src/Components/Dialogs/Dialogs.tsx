f419f64d078da7c295c6333b95320c9542c3547d
Nikolas-Bars <kasp2409@mail.ru>
2022-01-20 22:51:30 +0300

    lesson 21 dialogs

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import s from './../Dialogs/Dialogs.module.css'
 
-const Dialogs =()=>{
-    return(
-        <div className={s.content}>
-            Dialogs
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Lena
+                </div>
+                <div className={s.dialog}>
+                    Leha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    What is your name?
+                </div>
+                <div className={s.message}>
+                    I love to eat!
+                </div>
+            </div>
+
         </div>
     )
 }
